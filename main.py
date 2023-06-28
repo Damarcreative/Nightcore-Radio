fb7a8d843fc1bdc04b8984c2e36f8efcae082a2b
Damar Jati <106896172+Damarcreative@users.noreply.github.com>
2023-06-28 10:42:44 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,67 @@
+import discord
+from discord.ext import commands
+import random
+import os
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+voice_client = None  # Menyimpan referensi voice client agar bisa diakses di seluruh bot
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def play(ctx):
+    global voice_client
+    
+    if ctx.author.voice and ctx.author.voice.channel:
+        channel = ctx.author.voice.channel
+        
+        if voice_client and voice_client.is_connected():
+            await voice_client.move_to(channel)
+        else:
+            voice_client = await channel.connect()
+        
+        music_folder = os.path.join(os.path.dirname(__file__), "nightcore")  # Nama folder tempat musik lofi disimpan
+        music_files = [file for file in os.listdir(music_folder) if file.endswith(".mp3")]
+        
+        if not music_files:
+            await ctx.send("Tidak ada file musik yang ditemukan.")
+            await voice_client.disconnect()
+            voice_client = None
+            return
+        
+        random.shuffle(music_files)
+        
+        while True:
+            for music_file in music_files:
+                music_path = os.path.join(music_folder, music_file)
+                
+                voice_client.play(discord.FFmpegPCMAudio(music_path), after=lambda e: print(f"Berhenti memutar: {e}") if e else None)
+                
+                while voice_client.is_playing():
+                    await asyncio.sleep(1)
+                
+            random.shuffle(music_files)
+            
+    else:
+        await ctx.send("Anda harus bergabung dengan saluran suara terlebih dahulu.")
+
+@bot.command()
+async def stop(ctx):
+    global voice_client
+    
+    if voice_client and voice_client.is_connected():
+        voice_client.stop()
+        await voice_client.disconnect()
+        voice_client = None
+        await ctx.send("Berhenti memutar musik dan meninggalkan saluran suara.")
+    else:
+        await ctx.send("Bot tidak sedang terhubung ke saluran suara.")
+
+bot.run('DISCORD_API_KEY')
